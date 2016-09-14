472187456983af57c9a7178776be2ee1cc19afec
Jorge Klemm <jorge@meuspedidos.com.br>
2016-09-14 11:13:56 -0300

    Pep-8 kkkk

--- 01-hello-world.asm
+++ 01-hello-world.asm
@@ -35,5 +35,5 @@ mystart:
 
 section .data
 
-  mymsg db "hello, world", 0xa  ; string with a carriage-return
-  mylen equ $-mymsg             ; string length in bytes
+    mymsg db "hello, world", 0xa  ; string with a carriage-return
+    mylen equ $-mymsg             ; string length in bytes
